ae74af00a6faf46b12ed1d9e3802bdade3514dcb
peter.robinson@jax.org <peter.robinson@jax.org>
2022-02-28 16:30:38 +0000

    rearrange

--- src/test/k_means_test.py
+++ src/test/k_means_test.py
@@ -1,6 +1,6 @@
-import numpy as np
 from unittest import TestCase
 from kernelkm import KernelKMeans
+import numpy as np
 
 
 class TestKMeans(TestCase):
@@ -8,12 +8,12 @@ class TestKMeans(TestCase):
     @classmethod
     def setUpClass(cls) -> None:
         ## Make 6x6 matrix
-        cls._mat = np.array([[0, 5, 6, 1,1,1], # Patient 1 similarities
-                        [5, 0,4, 1, 1, 1 ], # Patient 2 similarities
-                         [1, 1, 0 , 4, 4, 7 ], # Patient 3 similarities
-                          [1, 1, 1 , 0, 5, 5 ], # Patient 4 similarities
-                          [1, 1, 1 , 5, 0, 5 ], # Patient 5 similarities
-                         [1, 1, 1 , 5, 5, 0 ]]) # Patient 6 similarities
+        cls._mat = np.array([[0, 5, 6, 1, 1, 1], # Patient 1 similarities
+                             [5, 0, 4, 1, 1, 1], # Patient 2 similarities
+                             [6, 4, 0, 1, 1, 1], # Patient 3 similarities
+                             [1, 1, 1, 0, 5, 5], # Patient 4 similarities
+                             [1, 1, 1, 5, 0, 5], # Patient 5 similarities
+                             [1, 1, 1, 5, 5, 0]]) # Patient 6 similarities
         cls._labels = ["p1","p2","p3","p4","p5","p6"]
 
     def test_ctor(self):
