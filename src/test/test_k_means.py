45155917125bc4d50958f4b9b31748d0dd02ef26
justinreese@lbl.gov <justinreese@lbl.gov>
2022-03-03 20:33:46 +0000

    add libraries

--- src/test/test_k_means.py
+++ src/test/test_k_means.py
@@ -1,6 +1,10 @@
-import numpy as np
 from unittest import TestCase
+import numpy as np
 from kernelkm import KernelKMeans, GapStat
+import matplotlib.pyplot as plt
+from sklearn.datasets import make_blobs
+from scipy.spatial import distance_matrix
+import pandas as pd
 
 
 class TestKMeans(TestCase):
@@ -42,7 +46,44 @@ class TestKMeans(TestCase):
         k, _, _ = gstat.calculate_good_k()
         self.assertEqual(2, k)
 
+    def test_on_blob_data(self):
+        correct_num_clusters = 3
+        patient_IDs = ["patient" + str(i) for i in range(3)]
+        X, correct_clusters = make_blobs(n_samples=100, n_features=2,
+                                         centers=correct_num_clusters, cluster_std=.8,)[0]
 
+        # turn X in sim matrix to pass to kernelkm 
+        # Original code from OP, slightly reformatted
+        # DF_var = pd.DataFrame.from_dict({
+        #     "s1":[1.2,3.4,10.2],
+        #     "s2":[1.4,3.1,10.7],
+        #     "s3":[2.1,3.7,11.3],
+        #     "s4":[1.5,3.2,10.9]
+        # }).T
+        # DF_var.columns = ["g1","g2","g3"]
+
+        # Whole similarity algorithm in one line
+        X_sim = pd.DataFrame(
+            1 / (1 + distance_matrix(X, X)),
+            columns=patient_IDs, index=patient_IDs
+        )
+
+        #           g1        g2        g3
+        # g1  1.000000  0.215963  0.051408
+        # g2  0.215963  1.000000  0.063021
+        # g3  0.051408  0.063021  1.000000
+
+        plt.figure(figsize=(12, 3))
+        for k in range(1, 6):
+            # kmeans = KMeans(n_clusters=k)
+            # a = kmeans.fit_predict(X)
+            kmeans = KernelKMeans(datamat=np.array(X_sim), patient_id_list=patient_IDs)
+            centroids, centroid_assignments, errors = kmeans.calculate(k)
+            plt.subplot(1, 5, k)
+            plt.scatter(X[:, 0], X[:, 1], c=centroid_assignments)
+            plt.xlabel('k='+str(k))
+        plt.tight_layout()
+        plt.show()
 
 
 
