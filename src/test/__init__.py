fab2d77e746ea6289436062e476a0717a4b48857
justinreese@lbl.gov <justinreese@lbl.gov>
2022-03-03 21:04:01 +0000

    get rid of stuff in __init__.py

--- src/test/__init__.py
+++ src/test/__init__.py
@@ -1,10 +1,1 @@
-# from pyspark.sql import functions as F
-from transforms.api import transform_df, Input, Output
 
-
-@transform_df(
-    Output("/UNITE/[RP-2AE058] [N3C Operational] Machine-learning resources for N3C/Semantic_similarity_clustering/kernelkm/src/test/__init__"),
-    source_df=Input("SOURCE_DATASET_PATH"),
-)
-def compute(source_df):
-    return source_df
